1b2c7e671351973c318e8e4b5a9cb2cdea44bf61
sammysundar2001@gmail.com <samradnus2001@gmail.com>
2022-04-10 17:00:28 +0530

    Fixed Navbar and Updated CSS

--- src/components/secondary/Card.js
+++ src/components/secondary/Card.js
@@ -25,7 +25,7 @@ function Card(props) {
     const data2 = await fetch(url2);
     const parsedData2 = await data2.json();
     const trailer = parsedData2.videos.results.find(vid => vid.name === 'Official Trailer')
-
+ 
     const loc = trailer.key;
     setLoc(loc);
     console.log(id);
