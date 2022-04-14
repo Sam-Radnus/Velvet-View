a22d5170d349bc56accd7f9d5ccab14a8fa28ba0
samsundar <samradnus2001@gmail.com>
2022-04-14 08:24:28 +0530

    updated CSS

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,35 +5,26 @@ function navbar() {
         <>
             <nav style={{position:'fixed',top:'0',zIndex:'6',width:'100%'}} className="navbar navbar-default navbar-fixed-top navbar-expand-lg navbar-dark ">
                 <div className="container-fluid">
-                    <a  id="heading "style={{ marginRight: '10vw' }} className="navbar-brand " href="#">Movie<span>-Array</span></a>
+                    <a   id="heading "style={{ fontSize:'xx-large',marginRight: '10vw' }} className="navbar-brand " href="#">Movie<span>-Array</span></a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
-                            <a className="nav-link active mx-2" href="#">Movies</a>
-                            <a className="nav-link  mx-2" href="#">TV Shows</a>
-                            <a className="nav-link  mx-2" href="#">Animations</a>
-                            <a className="nav-link  mx-2 " href="#">Date Night</a>
-                            <a className="nav-link  mx-2 " href="#">Advanced Search</a>
+                            <a style={{fontSize:'large'}}className="nav-link active mx-2" href="#">Movies</a>
+                            <a style={{fontSize:'large'}}className="nav-link  mx-2" href="#">TV Shows</a>
+                            <a style={{fontSize:'large'}}className="nav-link  mx-2" href="#">Animations</a>
+                            <a style={{fontSize:'large'}}className="nav-link  mx-2 " href="#">Date Night</a>
+                            <a style={{fontSize:'large'}}className="nav-link  mx-2 " href="#">Advanced Search</a>
                           
                         </div>
                     </div>
-                    <div className='white'>
-                
-                        <i style={{ color: 'white', cursor: 'pointer', marginLeft: '15px' }} className="fa-solid fa-magnifying-glass"></i>
-                        <i style={{ color: 'white', cursor: 'pointer', marginLeft: '15px' }} className="fa-solid fa-arrows-rotate"></i>
-                        <i style={{ color: 'white', cursor: 'pointer', marginLeft: '15px' }} className="fa-brands fa-buffer"></i>
-                        <i style={{ color: 'white', cursor: 'pointer', marginLeft: '15px' }} className="fa-solid fa-circle-user"></i>
-                         
-                        <a className=" dropdown-toggle mx-3" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            
-                            </a>
-                            <ul className="dropdown-menu bg-dark" aria-labelledby="navbarDropdownMenuLink">
-                                <li><a className="dropdown-item" href="#">Action</a></li>
-                                <li><a className="dropdown-item" href="#">Another action</a></li>
-                                <li><a className="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
+                    <div style={{display:'flex',marginRight:'2vw'}} className='white'>
+                      
+                      <li>  <i style={{ color: 'white', fontSize:'large',cursor: 'pointer', marginLeft: '15px' }} className="fa-solid fa-magnifying-glass"></i></li>
+                      <li>  <i style={{ color: 'white', fontSize:'large',cursor: 'pointer', marginLeft: '15px' }} className="fa-solid fa-arrows-rotate"></i></li>
+                      <li>  <i style={{ color: 'white', fontSize:'large',cursor: 'pointer', marginLeft: '15px' }} className="fa-brands fa-buffer"></i></li>
+                      <li>  <i style={{ color: 'white', fontSize:'large',cursor: 'pointer', marginLeft: '15px' }} className="fa-solid fa-circle-user"></i></li>
                         
                     </div>
                 </div>
