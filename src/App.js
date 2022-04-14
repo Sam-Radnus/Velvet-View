63794e723ca9c727d4a022b24ccbcd8f1026fd80
samsundar <samradnus2001@gmail.com>
2022-04-14 07:46:18 +0530

    Bug Fixes and Code Refactoring

--- src/App.js
+++ src/App.js
@@ -6,34 +6,39 @@ import Video1 from './components/secondary/Video1';
 import { useState } from 'react';
 import NowPlaying from './components/NowPlaying';
 import Featured from './components/Featured';
-import { BrowserRouter as Router, Route, Routes, Link,useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import SeeAll from './components/info/SeeAll';
 import { useEffect } from 'react';
 function App() {
+  
   return (
     <>
-
+  
       <Router>
         <Navbar />
         <Sidebar />
+      
         
-        <div style={{ marginTop:`70vh`, marginLeft: '25vw' }}>
-        <NowPlaying />
+        
           <Routes>
          
           <Route exact path="/" element={<> 
-          
+            
+            <div style={{ marginTop:`${window.location.pathname==='/'?'70vh':'10vh'}`, marginLeft: '25vw' }}>
+            <NowPlaying />
           <Featured FeatureName={"TOP RATED"}    location={"TopRated"} FeatureURL="https://api.themoviedb.org/3/movie/top_rated?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-US" />
           <Featured FeatureName={"UPCOMING"}     location={"Upcoming"} FeatureURL="https://api.themoviedb.org/3/movie/upcoming?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-US" />
           <Featured FeatureName={"TRENDING"}     location={"Trending"} FeatureURL="https://api.themoviedb.org/3/trending/all/day?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-US" />
-          <Featured FeatureName={"RECOMMENDED"}  location={"Recommended"} FeatureURL="https://api.themoviedb.org/3/movie/11/recommendations?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-US&page=1" /></>}
+          <Featured FeatureName={"RECOMMENDED"}  location={"Recommended"} FeatureURL="https://api.themoviedb.org/3/movie/11/recommendations?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-US&page=1" />
+            </div>
+          </>}
          ></Route>
          <Route exact path="/TopRated" element={<SeeAll key="topRated" title="Top Rated" URL="https://api.themoviedb.org/3/movie/top_rated?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-US"></SeeAll>}></Route>
          <Route exact path="/Upcoming" element={<SeeAll key="upComing" title="Upcoming" URL="https://api.themoviedb.org/3/movie/upcoming?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-US"></SeeAll>}></Route>
          <Route exact path="/Trending" element={<SeeAll key="Trending" title="Trending" URL="https://api.themoviedb.org/3/trending/all/day?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-US"></SeeAll>}></Route>
          <Route exact path="/Recommended" element={<SeeAll key="Recommended" title="Recommended" URL="https://api.themoviedb.org/3/movie/11/recommendations?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-US&page=1"></SeeAll>}></Route>
           </Routes>
-        </div>
+      
       </Router>
     </>
   );
--- src/components/info/SeeAll.js
+++ src/components/info/SeeAll.js
@@ -19,24 +19,25 @@ function SeeAll(props) {
         setMovie(parsedData.results);
     }
     return (
-        <>
-            <h1>{title}</h1>
+        <div style={{width:'75vw',marginLeft:'15vw'}} >
+          
             
-            <div style={{ marginTop: '0px' }} className='container'>
+            <div style={{ marginTop: '20vh',marginLeft:'10vw' }} className='container'>
+            <h1 style={{fontSize:'65px',textAlign:'center'}}>{title}</h1>
                 <div className='row row-cols-4'>
                     
-                    {movie.slice(2, 20).map(element => (
+                    {movie.slice(0, 20).map(element => (
                         <div style={{backgroundColor:'rgb(23,22,27)'}} className="card" >
                         <img src={`https://image.tmdb.org/t/p/original/${element.poster_path}`} className="card-img-top" />
                         <div className="card-body">
-                          <h5 className="card-title">{element.title}</h5>
+                          <h5 className="card-title">{element.title?element.title:element.name}</h5>
                           <a href="#" className="btn btn-danger">View More</a>
                         </div>
                       </div>
                     ))}
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
