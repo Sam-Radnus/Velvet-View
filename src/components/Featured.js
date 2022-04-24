7b1b72a985e4e5a3c2b7daa88df82bd1a61dd299
sammysundar2001@gmail.com <samradnus2001@gmail.com>
2022-04-24 11:08:40 +0530

    Code Refactoring

--- src/components/Featured.js
+++ src/components/Featured.js
@@ -5,7 +5,7 @@ import MidCard from './secondary/MidCard';
 import {useState,useEffect} from 'react';
 import '../App.css';
 function Featured(props) {
-    let {FeatureName,location,FeatureURL}=props;
+    let {FeatureName,limit,location,FeatureURL}=props;
 
     const [movie,setMovie]=useState([]);
    
@@ -30,7 +30,7 @@ function Featured(props) {
     <div  style={{marginTop:'1vh'}}className='container'>
    <div className="tag" style={{display:'inline',width:'100%'}}><p> <span style={{color:'white',fontSize:'50px'}}>{props.FeatureName}</span><Link style={{position:'absolute',textDecoration:'none',color:'grey',cursor:'pointer',marginTop:'30px',right:'2vw'}} to={`/${location}`}>See All <i className="fa-solid fa-angle-right"></i> </Link></p></div>
        <div className='row'>
-       {movie.slice(0,3).map(element=>(
+       {movie.slice(limit-3,limit).map(element=>(
           <div className="col">
           {movie.length > 0 && <MidCard imageURL={`https://image.tmdb.org/t/p/w300/${element.backdrop_path}`} title={element.title?element.title:element.name}/> }
            </div>
