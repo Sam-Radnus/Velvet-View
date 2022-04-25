4a4e08327e5990d22d6713a6cc58a9a6b68dd329
sammysundar2001@gmail.com <samradnus2001@gmail.com>
2022-04-25 13:23:41 +0530

    Added Anime Section

--- src/components/secondary/BiggerCard.js
+++ src/components/secondary/BiggerCard.js
@@ -7,7 +7,7 @@ function BiggerCard(props) {
       <h1>Now Playing</h1>
       <div style={{ height: '290px', width: '75vw', borderRadius:'10px',backgroundImage: `url(${props.imageURL} `,backgroundPosition:`85% 20%`}}>
         <div style={{ backgroundColor: 'rgba(0,0,0,0)', backdropFilter: 'blur(0px)' }} className="card-body">
-        
+         
            
         </div>
         <h1 id="title" style={{margin:'5% 0% 0% 1%',backgroundColor:'transparent',fontSize:'75px'}} >{props.title}</h1>
