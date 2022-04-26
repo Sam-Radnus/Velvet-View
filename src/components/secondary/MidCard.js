0e5354d1e6af033e5b3d369ae5c594d9f8dc5203
samsundar <samradnus2001@gmail.com>
2022-04-26 13:57:16 +0530

    Added Blur Effect

--- src/components/secondary/MidCard.js
+++ src/components/secondary/MidCard.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 import './Mid.css';
 function MidCard(props) {
     let { imageURL, title } = props;
@@ -8,7 +8,8 @@ function MidCard(props) {
            
             <div style={{ height: '170px', width: '299px', position:'relative',borderRadius: '10px', backgroundImage: `url(${props.imageURL} ` }}>
                 <div  className="card-body">
-                    <div className='frost' style={{ position:'absolute',top:'71%',left:'0',borderRadius:'10px',height:'50px',width:'299px' }}  ><p id="title" style={{ fontSize: '15px'}} className="card-title">{props.title}</p></div>
+                <p id="title" style={{ position:'absolute',top:'75%',fontSize: '15px'}} className="card-title">{props.title}</p>
+                    <div className='frost' style={{ position:'absolute',top:'71%',left:'0',borderRadius:'10px',height:'50px',width:'299px'}}></div>
 
                 </div>
             </div>
