7a0ae22f3f20dcf9668970b9e6654b8c210b7e90
samsundar <samradnus2001@gmail.com>
2022-05-03 23:03:41 +0530

    Searching Enabled in Advanced Search

--- src/components/Features/AdvancedSearch.js
+++ src/components/Features/AdvancedSearch.js
@@ -15,25 +15,40 @@ const [showFilter,setShowfilter]=useState(true);
 const [showResults,setShowResults]=useState(false);
 const [startDate,setStartDate]=useState(1900);
 const [endDate,setEndDate]=useState(new Date().getFullYear());
+const [query,setQuery]=useState('batman');
 const [sortBy,setSortBy]=useState('popularity');
-const [rule,setRule]=useState('asc');//asc or desc
+const [rule,setRule]=useState('desc');//asc or desc
 const [URL1,setURL1]=useState(`https://api.themoviedb.org/3/discover/${media}?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-US&sort_by=${sortBy}.${rule}&include_adult=false&include_video=false&primary_release_date.gte=${startDate}-01-01&primary_release_date.lte=${endDate}-12-31&with_watch_monetization_types=flatrate`)
 useEffect(()=>{
   fetchData();
-},[danger,media,startDate,endDate,sortBy,rule,showResults]);
+},[media,query,startDate,endDate,sortBy,rule]);
 const fetchData=async()=>{
    setURL1(`https://api.themoviedb.org/3/discover/${media}?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-US&sort_by=${sortBy}.${rule}&include_adult=false&include_video=false&${media==='movie'?'primary_release_date':'first_air_date.'}.gte=${startDate}-01-01&${media==='movie'?'primary_release_date':'first_air_date'}.lte=${endDate}-12-31&with_watch_monetization_types=flatrate`);
   console.log(URL1);
   }
+  const fetchData2=async()=>{
+   
+    console.log(query);
+    setURL1(`https://api.themoviedb.org/3/search/${media}?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-US&query=${query}&page=1&include_adult=false`);
+    setShowResults(true);
+    setShowfilter(false);
+    console.log(URL1);
+   }
   return (
     <div>
         <h1 style={{marginTop:'15vh',marginLeft:'40vw'}}>ADVANCED SEARCH</h1>
-        
-        <div className='Filter'>
-         <div className={'button-group'}>
+        <button onClick={()=>{setShowfilter(true)}} style={{background:'transparent',color:'white',border:'none'}}> <i  style={{ color: 'white',marginRight:'10px',fontSize:'large',cursor: 'pointer', marginLeft: '15px' }} className="fa-brands fa-buffer"></i>Filter</button>
+        <div className={'button-group'}>
          <button type="button" id="button" onClick={()=>{setDanger(true);setMedia('movie')}} className={`btn btn-${danger?'danger':'light'}`}>Movies</button>
          <button type="button" id="button" onClick={()=>{setDanger(false);setMedia('tv')}} className={`btn btn-${danger?'light':'danger'}`}>TV Show</button>
          </div>
+         <div className='search'>
+      <input id="search" onChange={(e)=>{ setQuery(e.target.value)}} type='text' onFocus={()=>{setShowfilter(false)}} onBlur={()=>{setShowfilter(true)}} /><button onClick={fetchData2} >Search</button>
+      </div>
+      <div className='Filter'>
+        {showFilter?
+        <div>
+        
          <div className={'sort-group'}>
          <button type="button" id="button" onClick={()=>{setRule('asc')}} className={`btn btn-${rule==='asc'?'danger':'light'}`}>Increasing Order</button>
          <button type="button" id="button" onClick={()=>{setRule('desc')}} className={`btn btn-${rule==='desc'?'danger':'light'}`}>Decreasing Order</button>
@@ -57,8 +72,11 @@ const fetchData=async()=>{
           setShowResults(true);
           navigate('/AdvancedSearch');
         }}>Search {media}</button>
+          </div>
+         </div>:''}
+         
          </div>
-        </div>
+            
        {showResults && <SeeAllSuggestions medium={media} title={"Results"} URL={URL1}/>}
     </div>
   )
