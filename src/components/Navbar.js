4207271278faac0c198721f340b811638e2bb7c5
samsundar <samradnus2001@gmail.com>
2022-05-08 09:50:15 +0530

    Updated Routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,7 +21,7 @@ function Navbar() {
                     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
                             <Link style={{fontSize:'large',color:`${location.pathname.slice(0,6)==='/Movie'?'rgb(255,0,77)':'white'}`}}className={`nav-link ${location.pathname==='/'?'active':''} mx-2`} to="/">Movies</Link>
-                            <Link style={{fontSize:'large',color:`${location.pathname.slice(0,8)==='/TVshows'?'rgb(255,0,77)':'white'}`}}className={`nav-link ${location.pathname==='/TVshows'?'active':''} mx-2`} to="/TVshows">TV Shows</Link>
+                            <Link style={{fontSize:'large',color:`${location.pathname.slice(0,3)==='/TV'?'rgb(255,0,77)':'white'}`}}className={`nav-link ${location.pathname==='/TVshows'?'active':''} mx-2`} to="/TVshows">TV Shows</Link>
                             <Link style={{fontSize:'large',color:`${location.pathname.slice(0,6)==='/Anime'?'rgb(255,0,77)':'white'}`}}className={`nav-link ${location.pathname==='/Anime'?'active':''} mx-2`} to="/Anime">Anime</Link>
                             <Link style={{fontSize:'large',color:`${location.pathname.slice(0,10)==='/DateNight'?'rgb(255,0,77)':'white'}`}}className={`nav-link ${location.pathname==='/DateNight'?'active':''} mx-2`} to="/DateNight">Date Night</Link>
                             <Link style={{fontSize:'large',color:`${location.pathname.slice(0,15)==='/AdvancedSearch'?'rgb(255,0,77)':'white'}`}}className={`nav-link ${location.pathname==='/AdvancedSearch'?'active':''} mx-2`} to="/AdvancedSearch">Advanced Search</Link>
--- src/components/secondary/MidCard.js
+++ src/components/secondary/MidCard.js
@@ -4,9 +4,10 @@ import { useNavigate } from 'react-router-dom';
 import './Mid.css';
 function MidCard(props) {
     let { imageURL,Medium,media,Feature,id, title } = props;
+    
     let navigate=useNavigate();
     return (
-        <div style={{marginTop:'0vh',cursor:'pointer'}}  onClick={()=>{navigate(`/${Medium}/${Feature}/Details/${id}`)}}>
+        <div style={{marginTop:'0vh',cursor:'pointer'}}  onClick={()=>{media?navigate(`/${Medium}/${Feature}/Details/${media}/${id}`):navigate(`/${Medium}/${Feature}/Details/${id}`)}}>
            
             <div style={{ height: '170px', width: '299px', position:'relative',borderRadius: '10px', background:props.imageURL!==null?`url(${props.imageURL} `:`url(https://icon-library.com/images/netflix-icon-21_84051.jpg)` }}>
             <p id="title" style={{ position:'absolute',zIndex:'5',top:'87.6%',textAlign:'center',width:'100%',fontSize: '15px'}} className="card-title">{props.title}</p>
