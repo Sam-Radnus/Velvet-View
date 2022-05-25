6f190f494b4f9e0e4bc5296403e44c3f161dded7
samsundar <samradnus2001@gmail.com>
2022-05-25 15:10:47 +0530

    Bug Fixing

--- src/components/Trailers.js
+++ src/components/Trailers.js
@@ -20,10 +20,15 @@ function Trailers() {
          }) ;
     }
     useEffect(()=>{
-       
-       getMovies();
-   
-    },[])
+      // const url="https://api.themoviedb.org/3/discover/movie?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-US&sort_by=popularity.desc&include_adult=false&page=1&append_to_response=videos";
+      // const data=await fetch(url);
+      // const parsedData=await data.json();
+      // setMovie(parsedData.results);
+      fetch("https://api.themoviedb.org/3/discover/movie?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-US&sort_by=popularity.desc&include_adult=false&page=1&append_to_response=videos").
+      then((response)=>response.json()).
+      then((data)=>setMovie(data.results))
+       console.log(movie);
+    },[movie.length])
     useEffect(()=>{
       window.addEventListener("resize",handleResize,false);
       console.log(dimensions);
@@ -33,32 +38,21 @@ function Trailers() {
     
     const getMovies=async()=>{
         // const url="https://api.themoviedb.org/3/movie/latest?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-US";
-        const url="https://api.themoviedb.org/3/discover/movie?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-US&sort_by=popularity.desc&include_adult=false&page=1&append_to_response=videos";
-        const data=await fetch(url);
-        const parsedData=await data.json();
-        setMovie(parsedData.results);
        
-        
-        const url2=`https://api.themoviedb.org/3/movie/${parsedData.results[0].id}?api_key=2023616ed87a6faf2ec9cd6de24b46ed&append_to_response=videos`;
-        const data2=await fetch(url2);
-        const parsedData2=await data2.json();
+        //console.log(movie);
         
      }
      
   return (
     <>
-     {mobile ? <div style={{borderBottomStyle:'solid' ,borderBottomColor:'rgb(27,26,31)'}}>
-          {movie.slice(0,3).map(element=>(
+    {console.log(movie.length)}
+     { movie&&<div style={{borderBottomStyle:'solid' ,borderBottomColor:'rgb(27,26,31)'}}>
+        {movie.slice(0,3).map(element=>(
               <Card key={element.id} play={element.id} imageURL={`https://image.tmdb.org/t/p/w500/${element.backdrop_path}`} title={element.title}/>
-          ))}  </div>:<div style={{borderBottomStyle:'solid' ,borderBottomColor:'rgb(27,26,31)'}}>
-          {movie.slice(0,3).map(element=>(
-              // <Card key={element.id} play={element.id} imageURL={`https://image.tmdb.org/t/p/w500/${element.backdrop_path}`} title={element.title}/>
-             <div> <a href={element.id}>{element.title}</a>
-              <hr/>
-              </div>
-          ))}     
-    </div>
-    }
+          ))}  
+       
+          </div>
+          }
     </>
          
   )
