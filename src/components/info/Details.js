278b77284b88687d11873871087849430c6407c0
samsundar <samradnus2001@gmail.com>
2022-06-10 14:14:06 +0530

    Updated Details

--- src/components/info/Details.js
+++ src/components/info/Details.js
@@ -11,34 +11,38 @@ function Details(props) {
     let navigate=useNavigate();
 
     const getMovies = async () => {
-       
+    
         const url = `https://api.themoviedb.org/3/${media?media:'movie'}/${username}?api_key=2023616ed87a6faf2ec9cd6de24b46ed&language=en-US`;
         const data = await fetch(url);
      
         const parsedData = await data.json();
-      
+       
         setMovie(parsedData);
+        console.log(parsedData);
      
     }
     useEffect(() => {
+        
         getMovies();
     },[ movie.id && username]);
     return (
+       
         movie &&    <div style={{ height: '85vh',width: '120vw',backgroundRepeat:'no-repeat',backgroundSize:'100vw',backgroundImage: `${movie.backdrop_path!==null} `?`url(https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`:'https://lrmonline.com/wp-content/uploads/2020/08/disney-300-ppi.jpg?mrf-size=m' }}>
         {
-           <div style={{backgroundColor:'rgba(0,0,50,0.5)',height:'85vh',paddingBottom:'5vh'}}>
-              <div style={{position:'relative',float:'left'}} className='image'>
-                <img style={{marginTop:'20%',marginLeft:'40%'}}  src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} />
-                </div>
-                <div style={{ marginLeft:'25% ',paddingTop:'5%',width:'70vw'}}className='overview'>
-                <h1 style={{textAlign:'center',fontSize:'75px',marginTop:'0px'}}> {movie.title?movie.title:movie.name}({movie.release_date?movie.release_date.slice(0,4):''})</h1>
-                   <h2 style={{color:'white'}}>Overview</h2>
-                    <h5 style={{ color: 'white' }}>{movie.overview}</h5>
-                    <h5 style={{ color: 'white' }}>Popularity:<span style={{color:`${movie.popularity>60?'orange':'red'}`}}>{movie.popularity}</span></h5>
-                    <h5 style={{ color: 'white' }}>Movie Runtime:{movie.runtime} minutes</h5>
-                    <div className='genres'><h5>Genres:-</h5>{movie.genres?movie.genres.map(genre=><div className='genres'  style={{cursor:'pointer'}} id={genre.name} onClick={()=>{navigate(`/Genre/${media?media:'movie'}/${genre.name}/${genre.id}`)}}>{genre.name}</div>):''}</div>
-                </div>
-            </div>
+           
+           <div style={{position:'absolute',backgroundColor:'rgba(0,0,50,0.5)',border:'solid',height:'85vh',width:'100vw',paddingBottom:'5vh'}}>
+         
+           <div ><img  style={{height:'60%',width:'20%'}} src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} /></div>
+           <div style={{position:'absolute',top:0,left:'25%'}}> <h2 style={{textAlign:'center',fontSize:'5vh',lineHeight:'none'}}> {movie.title?movie.title:movie.name}({movie.release_date?movie.release_date.slice(0,4):''})</h2>
+           <h2 style={{color:'white',textAlign:'center'}}>Overview</h2>
+                      <span style={{ color: 'white' }}>{movie.overview}</span>
+                      <br/>
+                      <span style={{ color: 'white' }}>Popularity:<span style={{color:`${movie.popularity>60?'orange':'red'}`}}>{movie.popularity}</span></span>
+                      <br/>
+                      <span style={{ color: 'white' }}>{window.location.pathname.slice(24,29)==='movie'?`Runtime:${movie.runtime}Minutes`:`Episode Length:${movie.episode_run_time[0]} minutes`}</span>
+                      <br/>
+                      <div className='genres'><h5>Genres:-</h5>{movie.genres?movie.genres.map(genre=><div className='genres'  style={{cursor:'pointer'}} key={genre.name} id={genre.name} onClick={()=>{navigate(`/Genre/${media?media:'movie'}/${genre.name}/${genre.id}`)}}>{genre.name}</div>):''}</div></div>
+             </div>
     
     
         }
--- src/components/secondary/Genres.js
+++ src/components/secondary/Genres.js
@@ -48,7 +48,7 @@ function Genres(props) {
        <div><h1 style={{color:'rgb(255,0,70)'}} >Genres:-</h1>
        
          <div style={{marginLeft:'1%'}} className='row row-cols-2'>
-         {genres?genres.slice(0,18).map(genre=><div className='genres' key={genre.name}  style={{textAlign:'center',justifyContent:'center',display:'flex',alignItems:'center',width:'105px',cursor:'pointer'}} id={genre.name} onClick={()=>{navigate(`/Genre/movie/${genre.name}/${genre.id}`)}}>{genre.name}</div>):''}
+         {genres?genres.map(genre=><div className='genres' key={genre.name}  style={{textAlign:'center',justifyContent:'center',display:'flex',alignItems:'center',cursor:'pointer'}} id={genre.name} onClick={()=>{navigate(`/Genre/movie/${genre.name}/${genre.id}`)}}>{genre.name}</div>):''}
          </div>
          </div>:<div><h3 style={{color:'rgb(255,0,70)'}}className='feature'>Search through Genres</h3><Link style={{width:'50px'}} to="/Genres" id="trailer"><i className="fa-solid fa-arrow-right"></i></Link></div>
          }
